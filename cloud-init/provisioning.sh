0b5d497d2917e54ff9fd3a13105d35b353cfbb35
Wayne Lewis <wlewis@ospreydcs.com>
2026-08-20 14:40:58 -0700

    Add extra required git commands to get submodules

--- cloud-init/provisioning.sh
+++ cloud-init/provisioning.sh
@@ -95,7 +95,11 @@ rm -fr /opt/vm-setup
 
 # Optionally install the training collection
 if [[ "$INSTALL_COLLECTION" == "true" ]]; then
+    install -d -m 700 -o epics-dev -g epics-dev /home/epics-dev/training
     git clone -b "$COLLECTION_REPO_BRANCH" "$COLLECTION_REPO_URL" /home/epics-dev/training
+    cd /home/epics-dev/training
+    git submodule init
+    git submodule update
     cd /home/epics-dev/training/vm-setup
     ./update.sh ..
     cd /home/epics-dev
